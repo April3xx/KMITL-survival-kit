7c84416561f93acde8a6bc220b3c0509961d5a00
banana rose <31275335+sode20yrs@users.noreply.github.com>
2020-05-03 10:44:16 +0700

    change loop to while

--- lec6q3.cpp
+++ lec6q3.cpp
@@ -2,20 +2,21 @@
 #include<string.h>
 int main()
 {
-    int count;                                               //declre variables
-    char sentence[50];                                       //declre variables
-    printf("Enter sentence: ");                             //just print
-    scanf("%[^\n]", sentence);                              //get input                                         
+    int count;                                           //declre variables
+    char sentence[100];                                  //declre variables
+                                           
+    printf("Enter sentence: ");                          //just print
+    scanf("%[^\n]", sentence);
     
+    while(sentence[count]!= NULL){                      //while sentence isn't end do code 
 
-    for(count=0;count<50;count++)                          //count through sentence length
-    {
-        if(sentence[count]!='\0')                          //check if the sentence isn't end continue doing the job
-        {
-        printf("%c", sentence[count]);                     //print each char in sentence in the same line
-        if(count%10==9) printf("\n");                      //if count to 9 go new line eg.. 0  1  2  3  4  5  6  7  8  9
-        }                                                                                //10 11 12 13 14 15 16 17 18 19 and so on
-    }                                                      // u see those repeating 0 1 2 3 4 5 6 7 8 9 right? 
+        printf("%c", sentence[count]);                  //print each char in sentence in the same line
+        if(count%10==9) printf("\n");                   //if count to 9 go new line eg.. 
+                                                        // 0  1  2  3  4  5  6  7  8  9
+                                                        //10 11 12 13 14 15 16 17 18 19 and so on
+        count++;                           
+    }
+                                                           // u see those repeating 0 1 2 3 4 5 6 7 8 9 right? 
     return 0;                                              // the algo here is if count%(a+1)==a when a is the max number per line
 }
 // testcase >> You are the wind beneath my wings.
