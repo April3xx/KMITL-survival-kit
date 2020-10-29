db890f818ec9954bba23eaec0e93390f6aa8a315
April3xx <58010207@kmitl.ac.th>
2020-10-29 14:46:09 +0700

    อะไรไม่รู้

--- test.cpp
+++ test.cpp
@@ -1,19 +1,7 @@
 #include <stdio.h>
-#include <conio.h>
-#include<string.h>
-void test(int i){
-    i+=200;
-   // printf("i in func %d",i);
-    
-}
-
-int main(){
-
-  int i =20;
-
-  test(i);
-
-  printf("i in main%d",i);
-  
-  //pass by value
+#include <string.h>
+int main () {
+int i=9,j=10,*a=&i,*b=&j;
+*a = *a * *a +*b;
+printf("%d*****%d",*a,*b);
 }
